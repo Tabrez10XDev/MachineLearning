02bf2bc0f0869e150cddd6d2320cb2b46c775174
Lj <meet.tabrezt1@gmail.com>
2020-08-15 23:07:37 +0530

    Learned fit_transform()

--- KNN/tutorial_2.py
+++ KNN/tutorial_2.py
@@ -7,3 +7,16 @@ from sklearn import linear_model, preprocessing
 
 data = pd.read_csv("KNN\cars.data")
 print(data.head())
+
+le = preprocessing.LabelEncoder()
+buying = le.fit_transform(list(data["buying"]))
+maint = le.fit_transform(list(data["maint"]))
+door = le.fit_transform(list(data["door"]))
+persons = le.fit_transform(list(data["persons"]))
+lug_boot = le.fit_transform(list(data["lug_boot"]))
+cls = le.fit_transform(list(data["class"]))
+
+x = list(zip(buying,maint,door,persons,lug_boot))
+y = list(cls)
+
+x_train,x_test,y_train,y_test = sklearn.model_selection.train_test_split(x,y,test_size=0.1)
