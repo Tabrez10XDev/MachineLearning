2de1d903855694c4076537ea9476ea697d6e823a
Lj <meet.tabrezt1@gmail.com>
2020-08-16 22:11:47 +0530

    Implemented KNN

--- KNN/tutorial_2.py
+++ KNN/tutorial_2.py
@@ -2,11 +2,10 @@ import sklearn
 from sklearn.utils import shuffle
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
-import numpy as np
+#import numpy as np
 from sklearn import linear_model, preprocessing
 
 data = pd.read_csv("KNN\cars.data")
-print(data.head())
 
 le = preprocessing.LabelEncoder()
 buying = le.fit_transform(list(data["buying"]))
@@ -15,8 +14,18 @@ door = le.fit_transform(list(data["door"]))
 persons = le.fit_transform(list(data["persons"]))
 lug_boot = le.fit_transform(list(data["lug_boot"]))
 cls = le.fit_transform(list(data["class"]))
-
+#print(buying)
 x = list(zip(buying,maint,door,persons,lug_boot))
 y = list(cls)
 
 x_train,x_test,y_train,y_test = sklearn.model_selection.train_test_split(x,y,test_size=0.1)
+
+model = KNeighborsClassifier(n_neighbors=9)
+model.fit(x_train,y_train)
+acc = model.score(x_test,y_test)
+print(acc)
+predicted = model.predict(x_test)
+names = ["unacc","acc","good","vgood"]
+for i in range(len(x_test)):
+    print(names[predicted[i]],x_test[i],names[y_test[i]] )
+
